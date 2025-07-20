12ef0686a34b1c1b8c6116b71ad02c9aba5e2f0d
sennome <alecbartz@outlook.com>
2025-07-20 05:48:11 -0500

    we do a little refactoring...

--- lib/dogfishing/consumer.ex
+++ lib/dogfishing/consumer.ex
@@ -22,6 +22,84 @@ defmodule Dogfishing.Consumer do
     :ets.insert(:dogfishing, {:current_page, page})
   end
 
+  def api_handler() do
+      receive do
+          {interaction, data} ->
+            {_, messages} = Process.info(self(), :message_queue_len)
+            if messages > 0 do #we're already backed up! just skip it
+              api_handler()
+            else
+              Api.Interaction.edit_response(interaction, data)
+              api_handler()
+            end
+          _ -> nil
+      end
+  end
+
+  def start_new_game(interaction) do
+    #start inital response
+    Api.Interaction.create_response(interaction, %{type: 4, data: %{
+      flags: 32768,
+      components: [
+        %{
+          type: 10,
+          content: "Searching 0 articles..."
+        }
+      ]
+    }})
+
+    #spawn seperate process so we can send a bunch of messages
+    #this process will queue up messages in its mailbox and
+    #automatically skip them if the queue grows too long
+    api_sender = spawn(&Dogfishing.Consumer.api_handler/0)
+
+    page = Wikipedia.random_article(fn page -> page.views > 10000 end, 0, fn accumulator ->
+      data = %{
+        flags: 32768,
+        components: [
+          %{
+            type: 10,
+            content: "Searching #{accumulator} articles..."
+          }
+        ]
+      }
+
+      send(api_sender, {interaction, data})
+    end)
+
+    #generate layout for final screen showing all categories
+    buttons = page.categories
+    |> Enum.map(fn category ->
+    %{
+      type: 2,
+      label: category,
+      style: 5,
+      url: URI.encode("https://en.wikipedia.org/wiki/Category:" <> category)
+    }
+    end)
+    |> Enum.chunk_every(5)
+    |> Enum.map(fn row ->
+    %{
+      type: 1,
+      components: row
+    }
+    end)
+    response = %{
+    flags: 32768,
+    components: buttons
+    }
+
+    set_current_page(page)
+    page.categories
+    |> Enum.reduce("", fn category, accumulator ->
+      "`#{category}` #{accumulator}"
+    end)
+    |> then(fn categories_text ->
+      send(api_sender, {interaction, response})
+      Process.exit(api_sender, :normal)
+    end)
+  end
+
   def handle_event({:MESSAGE_CREATE, message, _ws_state}) do
     if is_map(current_page()) && !message.author.bot do
       dist = message.content
@@ -88,29 +166,11 @@ defmodule Dogfishing.Consumer do
 
   end
 
-  def handle_event({:INTERACTION_CREATE, %Interaction{data: %{custom_id: "next"}} = interaction, _ws_state}) do #button press
-    Api.Interaction.create_response(interaction, %{type: 5})
-    :timer.sleep(2000)
-    Api.Interaction.edit_response(interaction, %{content: "nothing here..."})
-    :timer.sleep(2000)
-    Api.Interaction.edit_response(interaction, %{content: "something here!"})
-  end
+  def handle_event({:INTERACTION_CREATE, %Interaction{data: %{custom_id: "next"}} = interaction, _ws_state}), do: start_new_game(interaction)
 
-  def api_handler() do
-      receive do
-          {interaction, data} ->
-            {_, messages} = Process.info(self(), :message_queue_len)
-            if messages > 0 do #we're already backed up! just skip it
-              api_handler()
-            else
-              Api.Interaction.edit_response(interaction, data)
-              api_handler()
-            end
-          _ -> nil
-      end
-  end
+  def handle_event({:INTERACTION_CREATE, %Interaction{data: %{name: "next"}} = interaction, _ws_state}), do: start_new_game(interaction)
 
-  def handle_event({:INTERACTION_CREATE, %Interaction{data: %{name: "next"}} = interaction, _ws_state}) do #command
+  def handle_event({:INTERACTION_CREATE, %Interaction{data: %{name: "next"}} = interaction, 1}) do #command
     Api.Interaction.create_response(interaction, %{type: 4, data: %{
       flags: 32768,
       components: [
